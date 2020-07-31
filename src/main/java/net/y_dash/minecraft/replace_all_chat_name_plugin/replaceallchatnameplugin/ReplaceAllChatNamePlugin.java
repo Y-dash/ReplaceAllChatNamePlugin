33ed30ef0256c1b7e651f52206771bda308dbbb3
Y-dash <yokokura-yusuke@y-dash.net>
2020-08-01 07:26:20 +0900

    init project

---
+++ src/main/java/net/y_dash/minecraft/replace_all_chat_name_plugin/replaceallchatnameplugin/ReplaceAllChatNamePlugin.java
@@ -0,0 +1,17 @@
+package net.y_dash.minecraft.replace_all_chat_name_plugin.replaceallchatnameplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ReplaceAllChatNamePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
